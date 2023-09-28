77b3aef570c61975037fbe3832fb4476bb54df54
antonhtmlito <antondong04@gmail.com>
2023-09-28 17:28:31 +0200

    Slå to ens for at vinde spil

--- DiceRoll.java
+++ DiceRoll.java
@@ -11,7 +11,7 @@ public class DiceRoll {
     // ændring
     private boolean priviousRollTwoSixes;
     private boolean extraTurn;
-
+    private Scanner scan;
     public DiceRoll() {
         sum = 0;
         p1Score = 0;
@@ -70,9 +70,10 @@ public class DiceRoll {
                     System.exit(0);
                 } else
                     System.out.println("congratulation player 2, you won the game by drawing to sixs in a row");
-                System.out.println("Press enter to exit game");
-                System.exit(0);
+                    System.out.println("Press enter to exit game");
+                    System.exit(0);
             }
+            checkWinner();
             priviousRollTwoSixes = true;
         } else if (d1 == d2) {
             System.out.println("The dice rolled the same number " + d1 + ", roll again.");
@@ -89,14 +90,18 @@ public class DiceRoll {
     }
     
     public void checkWinner() {
-        if (getp1Score() >= 40) {
+        
+        
+        if (getp1Score() >= 40 && (p1Turn)) {
             System.out.println("Player 1 wins the game!");
             System.out.println("Press enter to exit game");
+            scan.close();
             System.exit(0);
         }
-        if (getp2Score() >= 40) {
+        if (getp2Score() >= 40 && (!p1Turn)) {
             System.out.println("Player 2 wins the game!");;
             System.out.println("Press enter to exit game");
+            scan.close();
             System.exit(0);
         }
     }
@@ -127,16 +132,12 @@ public class DiceRoll {
 
     public static void main(String[] args) {
         DiceRoll diceRoll = new DiceRoll();
-        var scan = new Scanner(System.in);
+        diceRoll.scan = new Scanner(System.in);
 
         System.out.println("DiceGame" + "\n" + "Press enter to play");
 
         while (true) {
-            scan.nextLine();
-
-            if (diceRoll.getp1Score() >= 40 || diceRoll.getp2Score() >= 40) {
-                break;
-            }
+            diceRoll.scan.nextLine();
 
             diceRoll.rollDice();
 
@@ -152,10 +153,6 @@ public class DiceRoll {
                 p1Turn = true;
             }
 
-            diceRoll.checkWinner();
-
         }
-
-        scan.close();
     }
 }
